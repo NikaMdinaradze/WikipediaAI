e9fea753e093cdcae76fa0b3347915df2aa37f29
NikaMdinaradze <mdinaradzenika04@gmail.com>
2024-03-06 17:48:16 +0400

    Add data retrieve endpoint

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from fastapi import BackgroundTasks, FastAPI, status
 
+from src.db import collection
 from src.schemas import SearchResponse
-from src.tasks import retrieve_topic_data
+from src.tasks import retrieve_summarize_write_task
 from src.wikipedia import search_topic
 
 app = FastAPI()
@@ -15,8 +16,18 @@ async def topic_search(topic: str):
 
 @app.post("/initiation")
 async def retrieval_initiation(page_id: int, background_tasks: BackgroundTasks):
-    background_tasks.add_task(retrieve_topic_data, page_id)
+    background_tasks.add_task(retrieve_summarize_write_task, page_id)
     return {
         "status": status.HTTP_202_ACCEPTED,
         "detail": "initiation successfully accepted",
     }
+
+
+@app.get("/retrieve")
+async def retrieve_summarized_topic(page_id: int):
+    document = await collection.find_one({"page_id": page_id})
+    if document:
+        document["_id"] = str(document["_id"])
+        return document
+    else:
+        return None
